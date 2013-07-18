fd568d3d6a9945f920ef604846feea3c5f5870a0
Seyran Avanesyan <seyrana@silverobject.com>
2013-07-18 07:36:59 -0700

    working on image table

--- SO.SilList.Database/data/Tables/Image.sql
+++ SO.SilList.Database/data/Tables/Image.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [data].[Image](
-	[imageId] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[imageId] UNIQUEIDENTIFIER NOT NULL DEFAULT newid() , 
 	[name] [nvarchar](50) NULL,
 	[url] [nvarchar](50) NULL,
 	[path] [nvarchar](50) NULL,
@@ -13,14 +13,8 @@
 	[createdBy] [int] NULL,
 	[modifiedBy] [int] NULL,
 	[isActive] [bit] NULL,
- CONSTRAINT [PK_Image] PRIMARY KEY CLUSTERED 
-(
-	[imageId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_imageId]  DEFAULT (newid()) FOR [imageId]
-GO
-ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_created]  DEFAULT (getdate()) FOR [created]
-GO
-ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_modified]  DEFAULT (getdate()) FOR [modified]
+-- generated from SSMS
+-- CONSTRAINT [PK_Image] PRIMARY KEY CLUSTERED 
+
+    CONSTRAINT [PK_Image] PRIMARY KEY ([imageId])
+)
