0d25f440704c140267fa372a7b94a7b8b9b9ddec
Michael Paltayan <Michael@SilverObject.com>
2013-07-17 20:57:49 -0700

    SSDT changes

---
+++ SO.SilList.CodeGeneration/DbContexts/SilList/SilListDbContext.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
