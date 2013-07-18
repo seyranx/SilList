bdbc42abd5afbf8a78888e59664d29cbe6e1e121
Seyran Avanesyan <seyrana@silverobject.com>
2013-07-18 00:35:30 -0700

    Added ListingDetail table

---
+++ SO.SilList.Database/data/Tables/ListingDetail.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [data].[ListingDetail]
+(
+	[listingDetailId] UNIQUEIDENTIFIER NOT NULL  DEFAULT newid(), 
+    [startDate] DATE NULL DEFAULT getdate(), 
+    [endDate] DATE NULL DEFAULT getdate(), 
+    [isApproved] BIT NULL DEFAULT 0,
+	CONSTRAINT [PK_ListingDetail] PRIMARY KEY ([listingDetailId])
+)
