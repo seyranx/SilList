5441a6016c9e56e630cc1045ca161b565a2296fc
Seyran Avanesyan <seyrana@silverobject.com>
2013-07-18 01:56:53 -0700

    Added image table

---
+++ SO.SilList.Database/data/Tables/Image.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [data].[Image](
+	[imageId] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[name] [nvarchar](50) NULL,
+	[url] [nvarchar](50) NULL,
+	[path] [nvarchar](50) NULL,
+	[fileType] [nvarchar](50) NULL,
+	[siteId] [int] NULL,
+	[height] [int] NULL,
+	[width] [int] NULL,
+	[size] [int] NULL,
+	[created] [datetime] NOT NULL,
+	[modified] [datetime] NOT NULL,
+	[createdBy] [int] NULL,
+	[modifiedBy] [int] NULL,
+	[isActive] [bit] NULL,
+ CONSTRAINT [PK_Image] PRIMARY KEY CLUSTERED 
+(
+	[imageId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_imageId]  DEFAULT (newid()) FOR [imageId]
+GO
+ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_created]  DEFAULT (getdate()) FOR [created]
+GO
+ALTER TABLE [data].[Image] ADD  CONSTRAINT [DF_Image_modified]  DEFAULT (getdate()) FOR [modified]
