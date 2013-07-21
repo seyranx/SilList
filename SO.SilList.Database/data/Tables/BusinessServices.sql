cc7f7564eab348a4a51c4301f37011dba704c99f
Hayk Khachikyan <haykk@silverobject.com>
2013-07-21 13:08:07 -0700

    Primary keys have been deleted.

--- SO.SilList.Database/data/Tables/BusinessServices.sql
+++ SO.SilList.Database/data/Tables/BusinessServices.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [data].[BusinessServices]
 (
-	[serviceTypeId] INT NOT NULL PRIMARY KEY, 
-    [businessId] UNIQUEIDENTIFIER NULL, 
+	[serviceTypeId] INT NOT NULL, 
+    [businessId] UNIQUEIDENTIFIER NOT NULL, 
     [created] DATETIME NULL DEFAULT getdate(), 
     [modified] DATETIME NULL DEFAULT getdate(), 
     [createdBy] INT NULL, 
