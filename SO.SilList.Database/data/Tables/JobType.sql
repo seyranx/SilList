875a519de9dfc58a4ad3b25593bc7cbd1d2a182d
Sako Stepanyan <sakos@silverobject.com>
2013-07-20 21:32:22 -0700

    DB Tables creation

---
+++ SO.SilList.Database/data/Tables/JobType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table3]
+(
+	[jobTypeId] INT NOT NULL PRIMARY KEY, 
+    [name] NVARCHAR(50) NULL, 
+    [description] NVARCHAR(MAX) NULL, 
+    [created] DATETIME NULL DEFAULT getdate(), 
+    [modified] DATETIME NULL DEFAULT getdate(), 
+    [createdBy] INT NULL, 
+    [modifiedBy] INT NULL, 
+    [isActive] BIT NULL
+)
