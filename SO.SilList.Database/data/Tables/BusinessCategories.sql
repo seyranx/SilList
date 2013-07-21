97f06a74286e18002ee053eff423b872711d5f3a
Edvin Zohrabyan <edvinz@silverobject.com>
2013-07-21 10:18:01 -0700

    added common columns in BusinessCategories table

--- SO.SilList.Database/data/Tables/BusinessCategories.sql
+++ SO.SilList.Database/data/Tables/BusinessCategories.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [data].[BusinessCategories] (
     [businessId]             UNIQUEIDENTIFIER NOT NULL,
     [businessCategoryTypeId] INT              NOT NULL,
+	[created] DATETIME NOT NULL DEFAULT getdate(), 
+    [modified] DATETIME NOT NULL DEFAULT getdate(), 
+    [createdBy] INT NULL, 
+    [modifiedBy] INT NULL, 
+    [isActive] BIT NULL
     CONSTRAINT [PK_BusinessCategories] PRIMARY KEY CLUSTERED ([businessId] ASC),
     CONSTRAINT [FK_BusinessCategories_BusinessCategoryType] FOREIGN KEY ([businessCategoryTypeId]) REFERENCES [app].[BusinessCategoryType] ([businessCategoryTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
