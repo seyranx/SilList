3ce8f6a8725e8de8ed3b316f9e8461794ea6cbe5
Vazric <vazricv@silverobject.com>
2013-07-21 10:37:54 -0700

    JpbType has been moved to app schema
    standar colums added too RentalUmage table

--- SO.SilList.Database/data/Tables/RentalImages.sql
+++ SO.SilList.Database/data/Tables/RentalImages.sql
@@ -1,5 +1,10 @@
 ﻿CREATE TABLE [data].[RentalImages]
 (
 	[imageId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [rentalId] UNIQUEIDENTIFIER NOT NULL
+    [rentalId] UNIQUEIDENTIFIER NOT NULL,
+	    [created] DATETIME NOT NULL DEFAULT getdate(), 
+    [modified] DATETIME NOT NULL DEFAULT getdate(), 
+    [createdBy] INT NULL, 
+    [modifiedBy] INT NULL, 
+    [isActive] BIT NULL
 )
