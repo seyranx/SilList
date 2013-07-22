28be09d76619447e1fe86a8b759d0336a7cb99b1
Tigran Makaryan <tigranm@silverobject.com>
2013-07-21 23:43:36 -0700

    Added RentalImages

--- SO.SilList.Database/app/Tables/RentType.sql
+++ SO.SilList.Database/app/Tables/RentType.sql
@@ -2,10 +2,10 @@
 (
 	[rentTypeId] INT NOT NULL PRIMARY KEY, 
     [name] NVARCHAR(50) NOT NULL, 
-    [description] NVARCHAR(50) NOT NULL,
+    [description] NVARCHAR(MAX) NOT NULL,
 	[created] DATETIME NOT NULL DEFAULT getdate(), 
     [modified] DATETIME NOT NULL DEFAULT getdate(), 
     [createdBy] INT NULL, 
     [modifiedBy] INT NULL, 
-    [isActive] BIT NULL
+    [isActive] BIT NULL DEFAULT 1
 )
