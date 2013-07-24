2171bca46c775088ad26588e8ed2b0df6ced6cea
Marat Astvatsatryan <marata@silverobject.com>
2013-07-23 23:31:57 -0700

    New Tables

---
+++ SO.SilList.Database/app/Tables/JobCategories.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [app].[JobCategories]
+(
+	[jobCategoryTypeId] INT NOT NULL PRIMARY KEY, 
+    [jobId] UNIQUEIDENTIFIER NULL,
+	[created] DATETIME NOT NULL DEFAULT getdate(), 
+    [modified] DATETIME NOT NULL DEFAULT getdate(), 
+    [createdBy] INT NULL, 
+    [modifiedBy] INT NULL, 
+    [isActive] BIT NULL
+)
