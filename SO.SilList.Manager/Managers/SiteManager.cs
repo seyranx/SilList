ffd96b76706ffcecf76ccf306bab03fcd608d584
Michael Paltayan <Michael@SilverObject.com>
2013-07-24 22:00:55 -0700

    SiteManager

---
+++ SO.SilList.Manager/Managers/SiteManager.cs
@@ -0,0 +1,44 @@
+﻿using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Interfaces;
+using SO.SilList.Manager.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SO.SilList.Manager.Managers
+{
+    public class SiteManager : ISiteManager
+    {
+        public SiteVo get(int siteId)
+        {
+            using (var db = new MainDb())
+            {
+                var result = db.sites
+                            .FirstOrDefault(s => s.siteId == siteId);
+                return result;
+            }
+        }
+
+        public List<SiteVo> getAll(bool? isActive = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(int siteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SiteVo update(SiteVo input, int? siteId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SiteVo insert(SiteVo input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
