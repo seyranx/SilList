1df272f5fa8c5961a4ef936861463786b72b7fc0
Edvin Zohrabyan <edvinz@silverobject.com>
2013-07-24 20:09:59 -0700

    Merge branch 'master' of bitbucket.org:SilverObject/so.sillist
    Conflicts:
    	SO.SilList.CodeGeneration/App.config
    	SO.SilList.CodeGeneration/DbContexts/SilList/Business.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessCategory.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessCategoryTypeVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessCategoryVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessImage.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessImageVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessRating.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessRatingVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessService.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessServiceVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/BusinessVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/Image.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/ImageVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/JobCompanyVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/JobTypeVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/JobVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/LeaseTermTypeVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/ListingDetailVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/MemberVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/Model1.Context.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/Model1.Designer.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/Model1.edmx
    	SO.SilList.CodeGeneration/DbContexts/SilList/Model1.edmx.diagram
    	SO.SilList.CodeGeneration/DbContexts/SilList/PropertyTypeVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/RatingVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/RentTypeVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/RentalImageVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/RentalVo.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/ServiceType.cs
    	SO.SilList.CodeGeneration/DbContexts/SilList/ServiceTypeVo.cs

---
+++ SO.SilList.Manager/Models/ValueObjects/RatingVo.cs
@@ -0,0 +1,59 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SO.SilList.CodeGeneration.DbContexts.SilList
+{
+     
+    [Table("Rating", Schema = "data" )]
+    [Serializable]
+    public partial class RatingVo
+    {
+    		
+    	[DisplayName("rating Id")]
+    	[Key]
+        public System.Guid ratingId { get; set; }
+    		
+    	[DisplayName("rating1")]
+        public Nullable<int> rating1 { get; set; }
+    		
+    	[DisplayName("review")]
+    	[StringLength(50)]
+        public string review { get; set; }
+    		
+    	[DisplayName("member Id")]
+        public Nullable<int> memberId { get; set; }
+    		
+    	[DisplayName("created")]
+    	[Required]
+        public System.DateTime created { get; set; }
+    		
+    	[DisplayName("modified")]
+    	[Required]
+        public System.DateTime modified { get; set; }
+    		
+    	[DisplayName("created By")]
+        public Nullable<int> createdBy { get; set; }
+    		
+    	[DisplayName("modified By")]
+        public Nullable<int> modifiedBy { get; set; }
+    		
+    	[DisplayName("is Active")]
+        public Nullable<bool> isActive { get; set; }
+      
+    	public RatingVo(){
+    			
+    	 this.ratingId = Guid.NewGuid();
+    	
+    	 this.isActive = true;
+    	}
+    }
+}
