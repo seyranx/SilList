a9fb6ee4f7dcf045eec57275d0666333a6b6fd32
Seyran Avanesyan <seyrana@silverobject.com>
2013-07-27 05:47:34 -0700

    Created Interfaces and managers. #13

---
+++ SO.SilList.Manager/Managers/ImageManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EntityFramework.Extensions;
+using SO.SilList.Manager.Interfaces;
+using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Models.ValueObjects;
+
+
+namespace SO.SilList.Manager.Managers
+{
+    public class ImageManager : IImageManager
+    {
+        /// <summary>
+        /// Find Image matching the imageId (primary key)
+        /// </summary>
+        public ImageVo get(Guid imageId)
+        {
+            using (var db = new MainDb())
+            {
+                var res = db.images
+                            .Include(s => s.site)
+                            .FirstOrDefault(p => p.imageId == imageId);
+
+                return res;
+            }
+        }
+
+        public List<ImageVo> getAll(bool? isActive = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(Guid imageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ImageVo update(ImageVo input, Guid? imageId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ImageVo insert(ImageVo input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SO.SilList.Manager/Managers/ListingDetailManager.cs
@@ -0,0 +1,51 @@
+﻿using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Interfaces;
+using SO.SilList.Manager.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EntityFramework.Extensions;
+
+namespace SO.SilList.Manager.Managers
+{
+    public class ListingDetailManager : IListingDetailManager
+    {
+        /// <summary>
+        /// Find ListingDetail matching the listingDetailId (primary key)
+        /// </summary>
+        public ListingDetailVo get(Guid listingDetailId)
+        {
+            using (var db = new MainDb())
+            {
+                var res = db.listingDetails
+                            .Include(x => x.site)
+                            .FirstOrDefault(p => p.listingDetailId == listingDetailId);
+
+                return res;
+            }
+        }
+
+        public List<ListingDetailVo> getAll(bool? isActive = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(Guid listingDetailId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListingDetailVo update(ListingDetailVo input, Guid? listingDetailId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ListingDetailVo insert(ListingDetailVo input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
