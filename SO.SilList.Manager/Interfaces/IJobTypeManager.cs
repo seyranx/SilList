72798ba52d9c15e09f9342f457fdf67fe2aa67b7
Sako Stepanyan <sakos@silverobject.com>
2013-07-27 21:09:33 -0700

    JobType and JobCompany changes

---
+++ SO.SilList.Manager/Interfaces/IJobTypeManager.cs
@@ -0,0 +1,18 @@
+﻿using SO.SilList.Manager.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SO.SilList.Manager.Interfaces
+{
+    interface IJobTypeManager
+    {
+        JobTypeVo get(int jobTypeId);
+        List<JobTypeVo> getAll(bool? isActive = true);
+        bool delete(int jobTypeId);
+        JobTypeVo update(JobTypeVo input, int? jobTypeId = null);
+        JobTypeVo insert(JobTypeVo input);
+    }
+}
---
+++ SO.SilList.Manager/Managers/JobTypeManager.cs
@@ -0,0 +1,44 @@
+﻿using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Interfaces;
+using SO.SilList.Manager.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SO.SilList.Manager.Managers
+{
+    class JobTypeManager : IJobTypeManager
+    {
+        public JobTypeVo get(int jobTypeId)
+        {
+            using (var db = new MainDb())
+            {
+                var result = db.jobTypes
+                            .FirstOrDefault(s => s.jobTypeId == jobTypeId);
+                return result;
+            }
+        }
+
+        public List<JobTypeVo> getAll(bool? isActive = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(int jobTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobTypeVo update(JobTypeVo input, int? jobTypeId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobTypeVo insert(JobTypeVo input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
