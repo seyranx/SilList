fbba852e6c537d48a241a223db8464f8684d4a7e
Seyran Avanesyan <seyrana@silverobject.com>
2013-07-28 13:49:29 -0700

    commented out FK-s and Associations.  commented out FK Include code in some managers

--- SO.SilList.Manager/Managers/BusinessManager.cs
+++ SO.SilList.Manager/Managers/BusinessManager.cs
@@ -67,7 +67,7 @@ namespace SO.SilList.Manager.Managers
             using (var db = new MainDb())
             {
                 var list = db.businesses
-                             .Include(s => s.site)
+                             //.Include(s => s.site)
                              .Where(e => isActive==null || e.isActive == isActive )
                              .ToList();
 
