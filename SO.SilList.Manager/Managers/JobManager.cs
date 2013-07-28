8474406f171d579ce1dd6e1da12c393038e55cf6
Marat Astvatsatryan <marata@silverobject.com>
2013-07-28 13:44:35 -0700

    JobManager

--- SO.SilList.Manager/Managers/JobManager.cs
+++ SO.SilList.Manager/Managers/JobManager.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Interfaces;
+using SO.SilList.Manager.Models.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SO.SilList.Manager.Managers
 {
-    class JobManager
+  public  class JobManager : IJobManager
     {
+        public Models.ValueObjects.JobVo get(Guid jobId)
+        {
+            using (var db = new MainDb())
+            {
+                var result = db.job
+                            .FirstOrDefault(s => s.jobId == jobId);
+                return result;
+            }
+        }
+
+        public List<Models.ValueObjects.JobVo> getAll(bool? isActive = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(Guid jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ValueObjects.JobVo update(Models.ValueObjects.JobVo input, Guid? jobId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.ValueObjects.JobVo insert(Models.ValueObjects.JobVo input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
