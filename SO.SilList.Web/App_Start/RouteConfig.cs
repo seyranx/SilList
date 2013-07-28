34d14fe4cccb6f792ac352ac9bb13541be0e9bc9
Michael Paltayan <Michael@SilverObject.com>
2013-07-28 10:18:14 -0700

    fixing admin config

--- SO.SilList.Web/App_Start/RouteConfig.cs
+++ SO.SilList.Web/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace SO.SilList.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("Admin/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
