10ccfdf477d322eabf6593f03f9135a5cde341a5
Edvin Zohrabyan <edvinz@silverobject.com>
2013-07-30 00:04:55 -0700

    added unit tests - still need to fix some bugs

---
+++ SO.SilList.Tests/Managers/BusinessCategriesManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SO.SilList.Manager.Managers;
+using SO.SilList.Manager.Models.ValueObjects;
+
+namespace SO.SilList.Tests.Managers
+{
+    [TestClass]
+    public class BusinessCategriesManagerTest
+    {
+        private BusinessCategoriesManager businessCategoriesManager = new BusinessCategoriesManager();
+        private Guid? NULL;
+
+        [TestMethod]
+        public void getAllTest()
+        {
+
+            var res = businessCategoriesManager.getAll(null);
+
+            if (res != null)
+            {
+                Assert.IsTrue(true);
+            }
+            else Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void insertDeleteTest()
+        {
+            var vo = new BusinessCategoriesVo();
+            vo.createdBy = 3;
+            vo.modifiedBy = 4;
+            vo.businessCategoryTypeId = 7;
+            //vo.businessId = b524440fb1f542ce99295dfc8058b22e;
+            vo.businessId = NULL;
+
+            var result = businessCategoriesManager.insert(vo);
+            var result2 = businessCategoriesManager.get(result.businessCategoryId);
+
+            businessCategoriesManager.delete(result.businessCategoryId);
+
+            var result3 = businessCategoriesManager.get(result.businessCategoryId);
+
+            if (result != null && result2 != null && result3 == null) // && result2.businessCategoryId != Guid.Empty)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+                Assert.IsTrue(false);
+        }
+
+
+        public Guid? b524440fb1f542ce99295dfc8058b22e { get; set; }
+    }
+}
