9f10df6539b6cdd9752b0535006b4372bfd5ed75
Seyran Avanesyan <seyrana@silverobject.com>
2013-08-03 16:11:55 -0700

    cosmetic

--- SO.SilList.Manager/Models/ValueObjects/MemberVo.cs
+++ SO.SilList.Manager/Models/ValueObjects/MemberVo.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +11,6 @@ using System.Data.Entity;
 
 namespace SO.SilList.Manager.Models.ValueObjects
 {
-     
     [Table("Member", Schema = "data" )]
     [Serializable]
     public partial class MemberVo
