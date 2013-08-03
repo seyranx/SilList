5ca48b31f7c3b19f613f457e06fa287a547d83a5
Edvin Zohrabyan <edvinz@silverobject.com>
2013-08-02 23:17:41 -0700

    patched: added common columns to all Listing tables

--- SO.SilList.Database/data/Tables/ListingImages.sql
+++ SO.SilList.Database/data/Tables/ListingImages.sql
@@ -1,6 +1,11 @@
 ﻿CREATE TABLE [data].[ListingImages]
 (
-	[listingImagesId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+	[listingImagesId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT (newid()), 
     [imageId] UNIQUEIDENTIFIER NULL, 
-    [listingId] UNIQUEIDENTIFIER NULL
+    [listingId] UNIQUEIDENTIFIER NULL,
+	[createdBy ] INT NULL, 
+    [modifiedBy] INT NULL, 
+    [created] DATETIME NOT NULL DEFAULT (getdate()), 
+    [modified] DATETIME NOT NULL DEFAULT (getdate()), 
+    [isActive] BIT NOT NULL DEFAULT ((1))
 )
