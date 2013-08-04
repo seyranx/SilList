d04b1003cb16ea285e1a782913fa837a4c21d98e
Michael Paltayan <Michael@SilverObject.com>
2013-08-04 13:59:40 -0700

    added BusinessCategoryType controller test

---
+++ SO.SilList.Admin.Web/Controllers/BusinessCategoryTypeController.cs
@@ -0,0 +1,35 @@
+﻿using SO.SilList.Manager.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SO.SilList.Admin.Web.Controllers
+{
+    public class BusinessCategoryTypeController : Controller
+    {
+        private BusinessCategoryTypeManager businessCategoryTypeManager = new BusinessCategoryTypeManager();
+
+        //
+        // GET: /BusinessCategoryType/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult List()
+        {
+            var results = businessCategoryTypeManager.getAll(null);
+            return PartialView(results);
+        }
+
+
+        public ActionResult Menu()
+        {
+           return PartialView("../Business/_Menu");
+        }
+    }
+}
