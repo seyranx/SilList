3d9b1c6c2156657366db5bf002ad174d4b575dab
Tigran Grigoryan <tigrang@silverobject.com>
2013-08-04 11:56:23 -0700

    Admin table structure

---
+++ SO.SilList.Database/data/Tables/Admin.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Admin]
+(
+	[adminId]			INT IDENTITY (1, 1) NOT NULL,
+    [firstName]         NVARCHAR (50) NULL,
+    [lastName]          NVARCHAR (50) NULL,
+    [email]             NVARCHAR (50) NULL,
+    [username]          NVARCHAR (50) NULL,
+    [password]          NVARCHAR (50) NULL,
+    [phone]             NVARCHAR (50) NULL,
+    [isEmailConfirmed]  BIT           DEFAULT ((0)) NULL,
+    [ipAddress]         NVARCHAR (50) NULL,
+    [lastLogin]         DATETIME      DEFAULT (getdate()) NOT NULL,
+    [created]           DATETIME      DEFAULT (getdate()) NOT NULL,
+    [modified]          DATETIME      DEFAULT (getdate()) NOT NULL,
+    [createdBy]         INT           NULL,
+    [modifiedBy]        INT           NULL,
+    [isActive]          BIT           DEFAULT ((1)) NULL, 
+    CONSTRAINT [PK_Admin] PRIMARY KEY ([adminId]),
+);
