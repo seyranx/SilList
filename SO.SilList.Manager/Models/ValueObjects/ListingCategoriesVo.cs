200e49a1a59366416c5c8e3fdee9b6876332c349
Edvin Zohrabyan <edvinz@silverobject.com>
2013-08-04 00:04:34 -0700

    Generated Code and ValueObjects for Listing,ListingImages,ListingType,ListingCategoreies,ListingCategoryType

---
+++ SO.SilList.Manager/Models/ValueObjects/ListingCategoriesVo.cs
@@ -0,0 +1,55 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SO.SilList.Manager.Models.ValueObjects
+{
+     
+    [Table("ListingCategories", Schema = "data" )]
+    [Serializable]
+    public partial class ListingCategoriesVo
+    {
+    		
+    	[DisplayName("listing Category Id")]
+    	[Key]
+        public System.Guid listingCategoryId { get; set; }
+    		
+    	[DisplayName("listing Category Type Id")]
+        public int? listingCategoryTypeId { get; set; }
+    		
+    	[DisplayName("listing Id")]
+        public System.Guid? listingId { get; set; }
+    		
+    	[DisplayName("created By_")]
+        public int? createdBy { get; set; }
+    		
+    	[DisplayName("modified By")]
+        public int? modifiedBy { get; set; }
+    		
+    	[DisplayName("created")]
+    	[Required]
+        public System.DateTime created { get; set; }
+    		
+    	[DisplayName("modified")]
+    	[Required]
+        public System.DateTime modified { get; set; }
+    		
+    	[DisplayName("is Active")]
+    	[Required]
+        public bool isActive { get; set; }
+      
+    	public ListingCategoriesVo()
+        {    			
+    		this.listingCategoryId = Guid.NewGuid();
+    	    this.isActive = true;
+    	}
+    }
+}
---
+++ SO.SilList.Manager/Models/ValueObjects/ListingImagesVo.cs
@@ -0,0 +1,55 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace SO.SilList.Manager.Models.ValueObjects
+{
+     
+    [Table("ListingImage", Schema = "data" )]
+    [Serializable]
+    public partial class ListingImagesVo
+    {
+    		
+    	[DisplayName("listing Images Id")]
+    	[Key]
+        public System.Guid listingImagesId { get; set; }
+    		
+    	[DisplayName("image Id")]
+        public System.Guid? imageId { get; set; }
+    		
+    	[DisplayName("listing Id")]
+        public System.Guid? listingId { get; set; }
+    		
+    	[DisplayName("created By")]
+        public int? createdBy { get; set; }
+    		
+    	[DisplayName("modified By")]
+        public int? modifiedBy { get; set; }
+    		
+    	[DisplayName("created")]
+    	[Required]
+        public System.DateTime created { get; set; }
+    		
+    	[DisplayName("modified")]
+    	[Required]
+        public System.DateTime modified { get; set; }
+    		
+    	[DisplayName("is Active")]
+    	[Required]
+        public bool isActive { get; set; }
+      
+    	public ListingImagesVo()
+        {	
+    		this.listingImagesId = Guid.NewGuid();   	
+    	    this.isActive = true;
+    	}
+    }
+}
