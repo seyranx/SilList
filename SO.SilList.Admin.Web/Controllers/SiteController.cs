c2baeed3e7f92ca6d6e98a319c20cbbfd879b167
Sako Stepanyan <sakos@silverobject.com>
2013-08-07 21:05:03 -0700

    Business menu items

---
+++ SO.SilList.Admin.Web/Controllers/SiteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SO.SilList.Manager.Managers;
+
+namespace SO.SilList.Admin.Web.Controllers
+{
+    public class SiteController : Controller
+    {
+        private SiteManager siteManager = new SiteManager();
+
+        //
+        // GET: /Site/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult List()
+        {
+            var results = siteManager.getAll(null);
+            return PartialView(results);
+        }
+
+    }
+}
