45e9e79c662a3ebf7935c1e44b148f7ed7c62cd7
Edvin Zohrabyan <edvinz@silverobject.com>
2013-08-11 11:30:57 -0700

    updating database schema

--- SO.SilList.Database/data/Tables/Listing.sql
+++ SO.SilList.Database/data/Tables/Listing.sql
@@ -1,12 +1,22 @@
 ﻿CREATE TABLE [data].[Listing] (
-    [listingId]       UNIQUEIDENTIFIER NOT NULL,
+    [listingId]       UNIQUEIDENTIFIER CONSTRAINT [DF__Listing__listing__61316BF4] DEFAULT (newid()) NOT NULL,
     [listingDetailId] UNIQUEIDENTIFIER NULL,
     [title]           NVARCHAR (50)    NULL,
     [description]     NVARCHAR (50)    NULL,
     [siteId]          INT              NULL,
     [listingTypeId]   INT              NULL,
-    PRIMARY KEY CLUSTERED ([listingId] ASC)
+    [createdBy ]      INT              NULL,
+    [modifiedBy]      INT              NULL,
+    [created]         DATETIME         CONSTRAINT [DF__Listing__created__589C25F3] DEFAULT (getdate()) NOT NULL,
+    [modified]        DATETIME         CONSTRAINT [DF__Listing__modifie__59904A2C] DEFAULT (getdate()) NOT NULL,
+    [isActive]        BIT              CONSTRAINT [DF__Listing__isActiv__5A846E65] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK__Listing__5A0F3D990749C430] PRIMARY KEY CLUSTERED ([listingId] ASC),
+    CONSTRAINT [FK_Listing_ListingDetail] FOREIGN KEY ([listingDetailId]) REFERENCES [data].[ListingDetail] ([listingDetailId]),
+    CONSTRAINT [FK_Listing_ListingType] FOREIGN KEY ([listingTypeId]) REFERENCES [app].[ListingType] ([listingTypeId]),
+    CONSTRAINT [FK_Listing_Site] FOREIGN KEY ([siteId]) REFERENCES [app].[Site] ([siteId])
 );
+
+
 
 
 
--- SO.SilList.Database/data/Tables/Member.sql
+++ SO.SilList.Database/data/Tables/Member.sql
@@ -11,18 +11,20 @@
     [username]          NVARCHAR (50) NULL,
     [password]          NVARCHAR (50) NULL,
     [phone]             NVARCHAR (50) NULL,
-    [isEmailConfirmed]  BIT           DEFAULT ((0)) NULL,
+    [isEmailConfirmed]  BIT           CONSTRAINT [DF__Member__isEmailC__619B8048] DEFAULT ((0)) NULL,
     [ipAddress]         NVARCHAR (50) NULL,
-    [lastLogin]         DATETIME      DEFAULT (getdate()) NOT NULL,
-    [isEmailSubscribed] BIT           DEFAULT ((0)) NULL,
-    [created]           DATETIME      DEFAULT (getdate()) NOT NULL,
-    [modified]          DATETIME      DEFAULT (getdate()) NOT NULL,
+    [lastLogin]         DATETIME      CONSTRAINT [DF__Member__lastLogi__628FA481] DEFAULT (getdate()) NOT NULL,
+    [isEmailSubscribed] BIT           CONSTRAINT [DF__Member__isEmailS__6383C8BA] DEFAULT ((0)) NULL,
+    [created]           DATETIME      CONSTRAINT [DF__Member__created__6477ECF3] DEFAULT (getdate()) NOT NULL,
+    [modified]          DATETIME      CONSTRAINT [DF__Member__modified__656C112C] DEFAULT (getdate()) NOT NULL,
     [createdBy]         INT           NULL,
     [modifiedBy]        INT           NULL,
-    [isActive]          BIT           DEFAULT ((1)) NOT NULL,
+    [isActive]          BIT           CONSTRAINT [DF__Member__isActive__66603565] DEFAULT ((1)) NULL,
     CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED ([memberId] ASC),
     CONSTRAINT [FK_Member_Site] FOREIGN KEY ([siteId]) REFERENCES [app].[Site] ([siteId])
 );
+
+
 
 
 
