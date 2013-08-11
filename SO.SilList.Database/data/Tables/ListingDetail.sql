bcd98018ae4d034951bb0c6f86a1ca27992641c4
Edvin Zohrabyan <edvinz@silverobject.com>
2013-08-11 13:31:46 -0700

    deleting listingDetail fields in Car's View

---
+++ SO.SilList.Database/data/Tables/ListingDetail.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [data].[ListingDetail] (
+    [listingDetailId] UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [startDate]       DATE             DEFAULT (getdate()) NOT NULL,
+    [endDate]         DATE             DEFAULT (getdate()) NOT NULL,
+    [isApproved]      BIT              DEFAULT ((0)) NULL,
+    [created]         DATETIME         DEFAULT (getdate()) NOT NULL,
+    [modified]        DATETIME         DEFAULT (getdate()) NOT NULL,
+    [createdBy]       INT              NULL,
+    [modifiedBy]      INT              NULL,
+    [isActive]        BIT              DEFAULT ((1)) NULL,
+    CONSTRAINT [PK_ListingDetail] PRIMARY KEY CLUSTERED ([listingDetailId] ASC)
+);
+
