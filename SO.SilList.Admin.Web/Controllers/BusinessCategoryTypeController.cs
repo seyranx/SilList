edb5ee3d6aebd6d176c7caf5beba4b9e62440539
Sako Stepanyan <sakos@silverobject.com>
2013-08-11 21:00:54 -0700

    Business Category & Service Type changes

--- SO.SilList.Admin.Web/Controllers/BusinessCategoryTypeController.cs
+++ SO.SilList.Admin.Web/Controllers/BusinessCategoryTypeController.cs
@@ -1,4 +1,5 @@
 ﻿using SO.SilList.Manager.Managers;
+using SO.SilList.Manager.Models.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,58 @@ namespace SO.SilList.Admin.Web.Controllers
         public ActionResult Menu()
         {
            return PartialView("../Business/_Menu");
+        }
+
+        [HttpPost]
+        public ActionResult Create(BusinessCategoryTypeVo input)
+        {
+
+            if (this.ModelState.IsValid)
+            {
+
+                var item = businessCategoryTypeManager.insert(input);
+                return RedirectToAction("Index");
+            }
+
+
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, BusinessCategoryTypeVo input)
+        {
+
+            if (this.ModelState.IsValid)
+            {
+                var res = businessCategoryTypeManager.update(input, id);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public ActionResult Edit(int id)
+        {
+            var result = businessCategoryTypeManager.get(id);
+            return View(result);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var result = businessCategoryTypeManager.get(id);
+            return View(result);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            businessCategoryTypeManager.delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
