d4b7f45bb392feaf31f0c3b532ebcaa795b15f67
Seyran Avanesyan <seyrana@silverobject.com>
2013-08-20 16:49:32 -0700

    in progress (Delete site)

--- SO.SilList.Manager/Managers/SiteManager.cs
+++ SO.SilList.Manager/Managers/SiteManager.cs
@@ -33,6 +33,19 @@ namespace SO.SilList.Manager.Managers
             }
         }
 
+        /* this is useful but not working anyway
+         *  db.DeleteObject(cr);
+         *  db.SaveChanges(); 
+         *  instead of
+         *  db.car.DeleteOnCommit();
+         *  db.SubmitChanges();
+         *   http://stackoverflow.com/questions/4650684/deleteonsubmit-doesnt-exist
+         *   
+         *  Remove in Entity Framework 5 instead of DeleteOnSubmit()
+         *  This works with my version of EF !!!
+         *  http://stackoverflow.com/questions/17203869/entity-framework-beta-3-dtabase-first-does-not-give-deleteonsubmit
+         * 
+         */
         public bool delete(int siteId)
         {
            using(var db = new MainDb())
@@ -41,6 +54,89 @@ namespace SO.SilList.Manager.Managers
               //var res = db.sites
               //   .Where(e => e.siteId == siteId)
               //   .Delete();
+
+              /////////////////////////////////////////////////////////
+               var deleteSiteReferences_inCar =
+                     from cr in db.car
+                     where cr.siteId == siteId
+                     select cr;
+               foreach (var cr in deleteSiteReferences_inCar)
+               {
+                  db.car.Remove(cr);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+              /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InMember =
+                    from x in db.members
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InMember)
+               {
+                  db.members.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+
+               /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InBusinessCategoryType =
+                    from x in db.members
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InBusinessCategoryType)
+               {
+                  db.members.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+               /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InBusiness  =
+                    from x in db.businesses
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InBusiness )
+               {
+                  db.businesses.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+               /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InImage =
+                    from x in db.images
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InImage)
+               {
+                  db.images.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+               /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InListing =
+                    from x in db.listing
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InListing)
+               {
+                  db.listing.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+               /////////////////////////////////////////////////////////
+               var deleteSiteReferences_InRental =
+                    from x in db.rental
+                    where x.siteId == siteId
+                    select x;
+               foreach (var x in deleteSiteReferences_InRental)
+               {
+                  db.rental.Remove(x);  // 2. Deleteobject(cr); // 1. DeleteOnSubmit(cr);
+               }
+
+              // // // 
+              
+              try
+               {
+                  db.SaveChanges(); //  db.SubmitChanges();
+               }
+               catch (Exception e)
+               {
+                  Console.WriteLine(e);
+                  // Provide for exceptions.
+               }
               return true;
            }
         }
