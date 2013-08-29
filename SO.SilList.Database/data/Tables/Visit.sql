1fcfe53484f1982c20028c44b55b49b18211f926
Tigran Grigoryan <tigrang@silverobject.com>
2013-08-28 17:18:54 -0700

    crteate Visi table with value object

---
+++ SO.SilList.Database/data/Tables/Visit.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Visit]
+(
+	[visitId] UNIQUEIDENTIFIER NOT NULL,
+	[siteId] INT NOT NULL,
+    [ipAddress] VARCHAR(50) NOT NULL, 
+    [referrerUrl] VARCHAR(MAX) NULL, 
+    [visitTime] DATETIME NOT NULL DEFAULT (getdate()), 
+    [browser] VARCHAR(50) NULL, 
+    [controller] VARCHAR(50) NOT NULL, 
+    [action] VARCHAR(50) NOT NULL,     
+    CONSTRAINT [PK_Visit] PRIMARY KEY ([visitId]), 
+    CONSTRAINT [FK_Visit_Site] FOREIGN KEY ([siteId]) REFERENCES [app].[Site] ([siteId]) ON DELETE CASCADE ON UPDATE CASCADE
+)
---
+++ SO.SilList.Tests/Setup/SiteStat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SO.SilList.Manager.DbContexts;
+using SO.SilList.Manager.Models.ValueObjects;
+
+namespace SO.SilList.Tests.Setup
+{
+    [TestClass]
+    public class SiteStatTest
+    {
+        [TestMethod]
+        public void SetupDB()
+        {
+
+            using (var db = new MainDb())
+            {
+
+                // get a valid site id
+                var site = db.sites.FirstOrDefault();
+                Assert.IsTrue(null != site);
+                
+                // add some visits
+                var visit = new VisitVo();
+                visit.siteId = site.siteId;
+                visit.ipAddress = "192.168.1.1";
+                visit.referrerUrl = "silist.com";
+                visit.visitTime = new DateTime(2013, 8, 25, 12, 23, 56);
+                visit.browser = "IE 8";
+                visit.controller = "Home";
+                visit.action = "Index";
+
+                try
+                {
+                    db.visits.Add(visit);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }                
+            }
+        }
+    }
+}
