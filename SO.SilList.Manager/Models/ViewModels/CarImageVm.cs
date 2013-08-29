5f2c9c01d73bf40e70946e8ed10e6417bc3dc8e3
Seyran Avanesyan <seyrana@silverobject.com>
2013-08-28 21:15:13 -0700

    using view model approach

---
+++ SO.SilList.Manager/Models/ViewModels/CarImageVm.cs
@@ -0,0 +1,18 @@
+﻿using SO.SilList.Manager.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Will be used in Care Create/Edit to assist in image uploading
+
+namespace SO.SilList.Manager.Models.ViewModels
+{
+    public class CarImageVm
+    {
+        public CarVo car { get; set; }
+        public Guid ImageId1 { get; set; }
+        public Guid ImageId2 { get; set; }
+    }
+}
