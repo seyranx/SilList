a96135604d6f6949c2233b02dfd1689e16c34c1e
Seyran Avanesyan <seyrana@silverobject.com>
2013-09-04 20:17:41 -0700

    s

--- SO.SilList.Manager/Managers/ImageManager.cs
+++ SO.SilList.Manager/Managers/ImageManager.cs
@@ -223,11 +223,6 @@ namespace SO.SilList.Manager.Managers
         // for Edit pages (upload and insert image right away?)
         public void InsertImageAndCarImage(Guid carId, string imageName, string imageUrl, string uploadImageAbsFilePath)
         {
-            // those two moved to caller function
-            // string imageName = Path.GetFileNameWithoutExtension(uploadImageAbsFilePath);
-            // string imageUrl = Path.Combine("~/", GetBasePathFromConfig(), imageNameOnServer);
-
-            /// second method: shorter
             ImageVo imgVo = new ImageVo();
             imgVo.name = imageName;
             imgVo.path = uploadImageAbsFilePath;
