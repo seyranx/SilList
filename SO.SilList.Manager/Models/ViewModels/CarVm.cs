fd0a5aca8e0bc53fed88b33591e0e6d645422938
Seyran <seyran_a@yahoo.com>
2013-09-24 15:19:22 -0700

    s

--- SO.SilList.Manager/Models/ViewModels/CarVm.cs
+++ SO.SilList.Manager/Models/ViewModels/CarVm.cs
@@ -1,6 +1,9 @@
 ﻿using SO.SilList.Manager.Models.ValueObjects;
+using SO.SilList.Utility.Classes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,10 +29,19 @@ namespace SO.SilList.Manager.Models.ViewModels
 
     public class CarVm
     {
-        public CarVm()
-        {
+        public List<CarVo> result { get; set; }
+        public string keyword { get; set; }
+        public int? pageNumber { get; set; }
 
-        }
+        [DisplayName("isActive: ")]
+        public bool? isActive { get; set; }
+        public int totalCount { get; set; }
+        public string submitButton { get; set; }
+        public Paging paging;
+
+        public CarVo car { get; set; }
+        public List<CarImageCheckBoxInfo> imagesToRemove { get; set; }
+
         public CarVm(CarVo input)
         {
             car = input;
@@ -44,7 +56,92 @@ namespace SO.SilList.Manager.Models.ViewModels
             imagesToRemove.Add(carImgInfo);
 
         }
-        public CarVo car { get; set; }
-        public List<CarImageCheckBoxInfo> imagesToRemove { get; set; }
+
+        public int totalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling(totalCount / (Decimal)rowCount);
+            }
+           
+
+        }
+        public int next10Page
+        {
+            get
+            {
+                return (int)Math.Min((int)pageNumber+10,totalPages);
+            }
+        }
+        public int prev10Page
+        {
+            get
+            {
+                if (pageNumber - 10 <= 0)
+                    return 1;
+                else
+                    return (int)(pageNumber - 10);
+
+            }
+        }
+        public int startingLinkPage
+        {
+            get
+            {
+                if (pageNumber > (int)(pageLinkCount / 2))
+                {
+                    int tmp = (int)pageNumber - (int)(pageLinkCount / 2);
+                    if(tmp+pageLinkCount>totalPages)
+                    tmp = (totalPages - pageLinkCount);
+                    if (tmp <= 0)
+                        tmp = 1;
+                    return tmp;
+                }
+                else
+                    return 1;
+            }
+        }
+        public int endingLinkPage
+        {
+            get
+            {
+                if (startingLinkPage + pageLinkCount > totalPages)
+                    return (totalPages - startingLinkPage) + startingLinkPage;
+                else
+                    return startingLinkPage + pageLinkCount; ;
+            }
+        }
+        public int skip
+        {
+            get
+            {
+                if (pageNumber == null || pageNumber < 2 || rowCount < 1) return 0;
+
+                return ((int)(pageNumber - 1) * (int)rowCount);
+            }
+        }
+        public int pageLinkCount
+        {
+            get
+            {
+                return  3;
+            }
+        }
+        public int rowCount
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public CarVm()
+        {
+            pageNumber = 1;
+            this.result = new List<CarVo>();
+            paging = new Paging();
+        }
+
+ 
     }
 }
