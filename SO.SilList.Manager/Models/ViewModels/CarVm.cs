9e45f82f9ec9a1f260ff5f01d3b004cb7d5ff1dd
Seyran <seyran_a@yahoo.com>
2013-09-27 18:55:22 -0700

    added back missing parts (car image) removed probably by bad merge.

--- SO.SilList.Manager/Models/ViewModels/CarVm.cs
+++ SO.SilList.Manager/Models/ViewModels/CarVm.cs
@@ -10,27 +10,64 @@ using System.Threading.Tasks;
 
 namespace SO.SilList.Manager.Models.ViewModels
 {
+    public class CarImageCheckBoxInfo
+    {
+        public CarImageCheckBoxInfo()
+        {
+        }
+        public CarImageCheckBoxInfo(string imgIdStr, string imgUrlStr, bool isChecked = true)
+        {
+            this.imageIdStr = imgIdStr;
+            this.imageUrlStr = imgUrlStr;
+            this.imageIsChecked = isChecked;
+        }
+        public string imageIdStr { get; set; }
+        public string imageUrlStr { get; set; }
+        public bool imageIsChecked { get; set; }
+    };
+
     public class CarVm
     {
         public List<CarVo> result { get; set; }
         public string keyword { get; set; }
-     
+
 
         [DisplayName("isActive: ")]
         public bool? isActive { get; set; }
-      //  public int totalCount { get; set; }
+        //  public int totalCount { get; set; }
         public string submitButton { get; set; }
         public Paging paging;
+
+        public CarVo car { get; set; }
+        public List<CarImageCheckBoxInfo> imagesToRemove { get; set; }
 
         public CarVm()
         {
             //pageNumber = 1;
+            car = new CarVo();
             this.result = new List<CarVo>();
-
-            if(paging==null)
-            paging = new Paging();
+            if (paging == null)
+                paging = new Paging();
         }
 
+        public CarVm(CarVo input)
+        {
+            car = input;
+            this.result = new List<CarVo>();
+            if (paging == null)
+                paging = new Paging();
+        }
+
+        public void AddCarImageInfo(ImageVo carImageVo, bool isChecked = true)
+        {
+            if (imagesToRemove == null)
+            {
+                imagesToRemove = new List<CarImageCheckBoxInfo>();
+            }
+            CarImageCheckBoxInfo carImgInfo = new CarImageCheckBoxInfo(carImageVo.imageId.ToString(), carImageVo.url);
+            imagesToRemove.Add(carImgInfo);
+
+        }
     }
 }
 
