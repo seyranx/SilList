29a913c7716ec0dfed9799b88ba2200a944f3879
Tigran Grigoryan <tigrang@silverobject.com>
2013-10-07 14:13:55 -0700

    Member login

---
+++ SO.SilList.Manager/Models/ViewModels/Member/MemberLoginVm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SO.SilList.Manager.Models
+{
+    public class MemberLoginVm
+    {
+        [DisplayName("Email")]
+        [Required]
+        public string email { get; set; }
+
+        [DisplayName("Password")]
+        [Required]
+        public string password { get; set; }
+
+        [DisplayName("Remember Me")]
+        public bool rememberMe { get; set; }
+
+        public MemberLoginVm()
+        {
+            this.rememberMe = true;
+        }
+    }
+}
---
+++ SO.SilList.Web/Controllers/MemberController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SO.SilList.Manager.Managers;
+using SO.SilList.Manager.Models;
+using SO.SilList.Manager.Models.ValueObjects;
+
+namespace SO.SilList.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        private MemberManager memberManager = new MemberManager();
+
+        public ActionResult Manage(string username)
+        {
+            var member = memberManager.get(1);
+            return View(member);
+        }
+
+        [HttpPost]
+        public ActionResult Manage(MemberVo input)
+        {
+            if (this.ModelState.IsValid)
+            {
+                memberManager.update(input, 1);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(MemberLoginVm input)
+        {
+            if (this.ModelState.IsValid)
+            {
+                // validate user
+                if (CurrentMember.validateUser(input.email, input.password))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(input.email, input.rememberMe);
+                }
+                else
+                {
+                    this.ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var login = new MemberLoginVm();
+            return View(login);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            CurrentMember.member = null;
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
