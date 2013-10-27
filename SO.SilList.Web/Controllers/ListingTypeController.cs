f2a6b3dc1a9f841da45ce8f68a259e2c8cee93c3
Seyran Avanesyan <seyrana@silverobject.com>
2013-10-27 13:55:25 -0700

    cleanup after copy paste

--- SO.SilList.Web/Controllers/ListingTypeController.cs
+++ SO.SilList.Web/Controllers/ListingTypeController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using SO.SilList.Manager.Managers;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
