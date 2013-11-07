00077e7d2e4f929213f854ad7a647de990a1e1e3
Seyran Avanesyan <seyrana@silverobject.com>
2013-11-07 00:46:40 -0800

    fixing Job

--- SO.SilList.Admin.Web/Controllers/JobController.cs
+++ SO.SilList.Admin.Web/Controllers/JobController.cs
@@ -58,14 +58,15 @@ namespace SO.SilList.Admin.Web.Controllers
             JobVo vo = new JobVo();
             return View(vo);
         }
-        public ActionResult Edit()
+        public ActionResult Edit(Guid id)
         {
-            return View();
+            var result = jobManager.get(id);
+
+            return View(result);
         }
         [HttpPost]
         public ActionResult Edit(Guid id, JobVo input)
         {
-
             if (this.ModelState.IsValid)
             {
                 var res = jobManager.update(input, id);
