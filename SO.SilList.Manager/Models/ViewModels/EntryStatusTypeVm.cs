0f5d4f540d0e07d0b578aca1731fa8cdeb5449ac
Seyran Avanesyan 2 <seyran_a@yahoo.com>
2013-11-12 17:08:23 -0800

    Initial commit of EntryStatusTypeVm

---
+++ SO.SilList.Manager/Models/ViewModels/EntryStatusTypeVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SO.SilList.Manager.Models.ValueObjects;
+
+namespace SO.SilList.Manager.Models.ViewModels
+{
+    public class EntryStatusTypeVm
+    {
+        public EntryStatusTypeVm()
+        {
+            entryStatusType = new EntryStatusTypeVo();
+        }
+        public EntryStatusTypeVm(EntryStatusTypeVo input)
+        {
+            entryStatusType = input;
+        }
+        public EntryStatusTypeVo entryStatusType { get; set; }
+    }
+
+}
