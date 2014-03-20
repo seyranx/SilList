abe30ee4af4a90a17d755782ca533b475433724b
Michael Paltayan <michael5272@gmail.com>
2014-03-20 00:53:22 -0700

    sync db changes

--- SO.SilList.Database/app/Tables/CarModelType.sql
+++ SO.SilList.Database/app/Tables/CarModelType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [app].[CarModelType] (
     [carModelTypeId] INT            IDENTITY (1, 1) NOT NULL,
-    [makeTypeId]     INT            NULL,
+    [carMakeTypeId]  INT            NULL,
     [name]           NVARCHAR (50)  NULL,
     [description]    NVARCHAR (MAX) NULL,
     [created]        DATETIME       CONSTRAINT [DF__ModelType__creat__13F1F5EB] DEFAULT (getdate()) NOT NULL,
@@ -9,6 +9,8 @@
     [modifiedBy]     INT            NULL,
     [isActive]       BIT            CONSTRAINT [DF__ModelType__isAct__15DA3E5D] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK__ModelTyp__9244B3E7C827E8F9] PRIMARY KEY CLUSTERED ([carModelTypeId] ASC),
-    CONSTRAINT [FK_ModelType_MakeType] FOREIGN KEY ([makeTypeId]) REFERENCES [app].[CarMakeType] ([carMakeTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT [FK_ModelType_MakeType] FOREIGN KEY ([carMakeTypeId]) REFERENCES [app].[CarMakeType] ([carMakeTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
--- SO.SilList.Database/data/Tables/BusinessServiceLookup.sql
+++ SO.SilList.Database/data/Tables/BusinessServiceLookup.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [data].[BusinessServiceLookup] (
     [businessServiceLookupId] UNIQUEIDENTIFIER NOT NULL,
-    [serviceTypeId]           INT              NULL,
+    [businessServiceTypeId]   INT              NULL,
     [businessId]              UNIQUEIDENTIFIER NULL,
     [created]                 DATETIME         CONSTRAINT [DF__BusinessS__creat__59FA5E80] DEFAULT (getdate()) NULL,
     [modified]                DATETIME         CONSTRAINT [DF__BusinessS__modif__5AEE82B9] DEFAULT (getdate()) NULL,
@@ -9,6 +9,8 @@
     [isActive]                BIT              DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_BusinessServices] PRIMARY KEY CLUSTERED ([businessServiceLookupId] ASC),
     CONSTRAINT [FK_BusinessServices_Business] FOREIGN KEY ([businessId]) REFERENCES [data].[Business] ([businessId]) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT [FK_BusinessServices_ServiceType] FOREIGN KEY ([serviceTypeId]) REFERENCES [app].[BusinessServiceType] ([businessServiceTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT [FK_BusinessServices_ServiceType] FOREIGN KEY ([businessServiceTypeId]) REFERENCES [app].[BusinessServiceType] ([businessServiceTypeId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
 
