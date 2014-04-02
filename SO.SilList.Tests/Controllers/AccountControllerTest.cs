6651e243274503893486594069a39a137fdc6660
Seyran Avanesyan 2 <seyran_a@yahoo.com>
2014-04-02 14:40:37 -0700

    Sillist_1: implemented basic Authentication. Updated references.

---
+++ SO.SilList.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SO.SilList.Manager.Models.ViewModels;
+using SO.SilList.Web.Controllers;
+
+namespace SO.SilList.Web.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        //Todo: not complete.
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            RegisterVm reg = new RegisterVm();
+            ViewResult result = controller.Register(reg) as ViewResult;
+
+            // Assert
+            //Assert.AreEqual(, result.ViewBag.Message);
+            Assert.IsTrue(true);
+
+        }
+    }
+}
---
+++ SO.SilList.Tests/Managers/MemberManagerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SO.SilList.Manager.Managers;
+using SO.SilList.Manager.Models.ValueObjects;
+
+namespace SO.SilList.Web.Tests.Managers
+{
+    [TestClass]
+    public class MemberManagerTest
+    {
+        private MemberManager memberManager = new MemberManager();
+
+        [TestMethod]
+        public void insertTest()
+        {
+            MemberVo mem = new MemberVo();
+
+            mem.username = "TestUserName";
+            mem.password = "TestPassword";
+            mem.isActive = false;
+
+            mem.firstName = "TestUserFirstName";
+            mem.lastName = "TestUserLastName";
+
+
+            mem.email = "TestUserEmail";
+            mem.isEmailConfirmed = false;
+            mem.isEmailSubscribed = false;
+            mem.lastLogin = DateTime.Now;
+
+            var res = memberManager.insert(mem);
+
+            Assert.IsTrue(res!= null); // res.createdBy != null && res.createdBy != 0);
+        }
+    }
+}
