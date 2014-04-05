dab7a93777aee88ae233db0a1f2114dc0173f2b7
Hayk Khachikyan <haykk@silverobject.com>
2014-04-05 12:16:22 -0700

    Added DropDownController, DropDovnVm, and _DropDown partial view.

---
+++ SO.SilList.Manager/Models/ViewModels/DropDownVm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SO.SilList.Manager.Models.ViewModels
+{
+    public class DropDownVm
+    {
+        public DropDownVm();
+
+        public string dataTextField { get; set; }
+        public string dataValueField { get; set; }
+        public object htmlAttributes { get; }
+        public bool isMultiple { get; set; }
+        public IList items { get; set; }
+        public string optionLabel { get; set; }
+        public string propertyName { get; set; }
+        public object selectedValue { get; set; }
+    }
+}
+
+
