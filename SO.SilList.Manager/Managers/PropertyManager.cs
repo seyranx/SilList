3b401354ce7acd9aef3e50f28e8cc418b5c59c11
Edvin Zohrabyan <edvinz@silverobject.com>
2014-04-05 16:42:17 -0700

    fixing Layout, InnerLayout, Propert

--- SO.SilList.Manager/Managers/PropertyManager.cs
+++ SO.SilList.Manager/Managers/PropertyManager.cs
@@ -14,6 +14,7 @@ using SO.Utility.Models.ViewModels;
 using SO.Utility;
 using SO.Utility.Helpers;
 using SO.Utility.Extensions;
+using SO.SilList.Manager.Models.ViewModels;
 
 
 
@@ -131,6 +132,79 @@ namespace  SO.SilList.Manager.Managers
             using (var db = new MainDb())
             {
                 return db.properties.Count();
+            }
+        }
+
+        public PropertyVm search(PropertyVm input)
+        {
+            DateTime listingDate = new DateTime();
+            listingDate = DateTime.Today.Date;
+            if (input.listingDate != null)
+            {
+                switch (input.listingDate)
+                {
+                    case 0: //last 1 day
+                        listingDate = listingDate.Subtract(new TimeSpan(1, 0, 0, 0, 0));
+                        break;
+                    case 1: //last 3 days
+                        listingDate = listingDate.Subtract(new TimeSpan(3, 0, 0, 0, 0));
+                        break;
+                    case 2: //last 7 days
+                        listingDate = listingDate.Subtract(new TimeSpan(7, 0, 0, 0, 0));
+                        break;
+                    case 3: //2 weeks
+                        listingDate = listingDate.Subtract(new TimeSpan(14, 0, 0, 0, 0));
+                        break;
+                    case 4: // last month
+                        listingDate = listingDate.Subtract(new TimeSpan(31, 0, 0, 0, 0));
+                        break;
+                    case 5: // last Two month
+                        listingDate = listingDate.Subtract(new TimeSpan(62, 0, 0, 0, 0));
+                        break;
+                }
+            }
+
+            using (var db = new MainDb())
+            {
+                var query = db.properties
+                            .Include(r => r.propertyType)
+                            .Include(t => t.propertyListingType)
+                            //.Include(s => s.site)
+                            .Include(i => i.cityType)
+                            .Include(o => o.countryType)
+                            .Include(u => u.stateType)
+                            //.Include(w => w.entryStatusType)
+                            .OrderByDescending(b => b.created)
+                            .Where(e => (input.isActive == null || e.isActive == input.isActive)
+                                      && ((e.title.Contains(input.keyword) || string.IsNullOrEmpty(input.keyword))
+                                            || (e.title.Contains(input.keyword) || e.description.Contains(input.keyword) || string.IsNullOrEmpty(input.keyword)))
+                                      && (input.zip == null || e.zip == input.zip)
+                                      && (input.filter_cityTypeId == null || e.cityTypeId == input.filter_cityTypeId)
+                                      && (input.propertyTypeId == null || e.propertyTypeId == input.propertyTypeId)
+                                      && (input.propertyListingTypeId == null || e.propertyListingTypeId == input.propertyListingTypeId)
+                                      //&& (e.bedroomTypeId == input.bedroomTypeId || input.bedroomTypeId == null)
+                                      //&& (e.bathroomTypeId == input.bathroomTypeId || input.bathroomTypeId == null)
+                                      && ((e.price >= input.startingPrice || input.startingPrice == null)
+                                      && (e.price <= input.endingPrice || input.endingPrice == null))
+                                      //&& (input.acceptsSection8TypeId == null || e.acceptsSection8TypeId == input.acceptsSection8TypeId)
+                                      //&& (input.isPetAllowedTypeId == null || e.isPetAllowedTypeId == input.isPetAllowedTypeId)
+
+                                      /*
+                                      && (input.showPendingOnly == null || input.showPendingOnly == false || e.entryStatusType.name.Equals(EntryStatusTypeStrings.csPending)) 
+                                      && (input.siteId == null || e.siteId == input.siteId)
+                                      //&& (input.entryStatusTypeId == null || e.entryStatusType.entryStatusTypeId == input.entryStatusTypeId)
+                                      && (input.listingDate ==null || DateTime.Compare(e.startDate, listingDate) >= 0)
+                                       */
+                             );
+
+                //input.paging.totalCount = query.Count();
+                input.result = query
+                             .OrderBy(b => b.title)
+                             .Skip(input.paging.skip)
+                             .Take(input.paging.rowCount)
+                             .ToList();
+
+                return input;
             }
         }
 
